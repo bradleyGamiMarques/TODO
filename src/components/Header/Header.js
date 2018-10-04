5eb38b50482ea345c0a26f8dffafa32b3b962b99
b0m00oh <bradley.marques@walmart.com>
2018-10-04 11:51:11 -0500

    added in an assets folder and began work on the hamburger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import {ReactComponent as Logo} from '../../assets/icons/baseline-menu-40px.svg';
 
 class Header extends Component {
     state = {  }
+    displayAlert = () => {
+        alert('Hello there!');
+    }
     render() {
         return (
-            <header>
-                <h1 className={'headerTitle'}>Content</h1>
-                <h2>Organize your life</h2>
+            <header className={'header-flex-container'}>
+                <section className={'header-content-flex-container'}>    
+                    <h1 className={'headerTitle'}>To-Do-List</h1>
+                    <h2>Organize your life</h2>
+                </section>
+                <section className={'header-hamburger-menu-flex-container'}>
+                <button onClick={()=> this.displayAlert()} className={'hamburger-menu-button'}><Logo/></button>
+                </section>
             </header>
         );
     }
